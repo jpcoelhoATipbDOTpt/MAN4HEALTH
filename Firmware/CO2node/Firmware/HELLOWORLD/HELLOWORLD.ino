700e5381cfaef87633408b12721f1c8d610ac276
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2023-02-19 17:05:37 +0000

    Update and rename HELLOWORLD.inio to HELLOWORLD.ino

--- Firmware/CO2node/Firmware/HELLOWORLD/HELLOWORLD.ino
+++ Firmware/CO2node/Firmware/HELLOWORLD/HELLOWORLD.ino
@@ -14,8 +14,8 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000); 
+  digitalWrite(LED, HIGH);               // desliga LED
+  delay(1000);                           // aguarda 1 segundo
+  digitalWrite(LED, LOW);                // liga LED
+  delay(1000);                           // aguarda 1 segundo
 }
