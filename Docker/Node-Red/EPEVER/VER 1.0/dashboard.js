f1ad98d279d65008e7b474c076bb4b5e7dee30cb
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2024-08-26 12:05:54 +0100

    Create dashboard.js

---
+++ Docker/Node-Red/EPEVER/VER 1.0/dashboard.js
@@ -0,0 +1,67 @@
+[
+    {
+        "id": "d9f1c2a084827a6c",
+        "type": "ui_chart",
+        "z": "c50bbe8c23efb028",
+        "name": "",
+        "group": "7abb7779e3d4bc3c",
+        "order": 3,
+        "width": "6",
+        "height": "4",
+        "label": "PV array Input Power",
+        "chartType": "line",
+        "legend": "false",
+        "xformat": "HH:mm:ss",
+        "interpolate": "linear",
+        "nodata": "",
+        "dot": false,
+        "ymin": "0",
+        "ymax": "50",
+        "removeOlder": 1,
+        "removeOlderPoints": "144",
+        "removeOlderUnit": "86400",
+        "cutout": 0,
+        "useOneColor": false,
+        "useUTC": false,
+        "colors": [
+            "#1f77b4",
+            "#aec7e8",
+            "#ff7f0e",
+            "#2ca02c",
+            "#98df8a",
+            "#d62728",
+            "#ff9896",
+            "#9467bd",
+            "#c5b0d5"
+        ],
+        "outputs": 1,
+        "useDifferentColor": false,
+        "className": "",
+        "x": 840,
+        "y": 40,
+        "wires": [
+            []
+        ]
+    },
+    {
+        "id": "7abb7779e3d4bc3c",
+        "type": "ui_group",
+        "name": "SOLAR PANNEL DATA",
+        "tab": "aadc49ba59317443",
+        "order": 2,
+        "disp": true,
+        "width": "6",
+        "collapse": false,
+        "className": ""
+    },
+    {
+        "id": "aadc49ba59317443",
+        "type": "ui_tab",
+        "z": "c50bbe8c23efb028",
+        "name": "BATTERY CHARGE CONTROLLER",
+        "icon": "dashboard",
+        "order": 1,
+        "disabled": false,
+        "hidden": false
+    }
+]
